ccfd821ed0770df68ba4493bb90b83b016af8f1c
chang-07 <schang.li@icloud.com>
2026-03-31 19:07:15 -0400

    fix(backend): correct sys.path in startup scripts to resolve ModuleNotFoundError

--- backend/scripts/reset_db.py
+++ backend/scripts/reset_db.py
@@ -1,7 +1,9 @@
 import os
 import django
+import sys
 
 # Setup Django environment
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vspp.settings')
 django.setup()
 
